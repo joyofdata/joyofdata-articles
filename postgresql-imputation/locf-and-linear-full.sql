b21c9e0c851add49d1454bb1a4421f2ad1126b2a
Raffael Vogler <raffael.vogler.de@gmail.com>
2015-10-11 21:41:38 +0200

    improved statement

--- postgresql-imputation/locf-and-linear-full.sql
+++ postgresql-imputation/locf-and-linear-full.sql
@@ -2,7 +2,7 @@ create or replace function locf_s(a float, b float)
 returns float
 language sql
 as '
-  SELECT COALESCE(b, a)
+  select coalesce(b, a)
 ';
 
 drop aggregate if exists locf(float);
@@ -13,33 +13,34 @@ create aggregate locf(float) (
 
 with 
 tbl0 as (
-    select a,b,t,v,
-           locf(v) over t_asc as x1,
-           locf(v) over t_desc as x2, 
+    select a, b, t, v,
+           locf(v) over t_asc as v_locf,
+           locf(v) over t_desc as v_focb, 
            locf(case when v is not null then t else null end) 
-                over t_desc as t2
+                over t_asc as t_locf,
+           locf(case when v is not null then t else null end) 
+                over t_desc as t_focb
     from tbl
     window t_asc as (partition by a,b order by t),
            t_desc as (partition by a,b order by t desc)
 ),
 tbl1 as (
-    select a,b,t,v,x1,x2,t2,
+    select a, b, t, v, v_locf, v_focb, t_locf, t_focb,
         case
             when 
-                t2 != min(t) over imputed 
-                and x1 is not null 
-                and x2 is not null
+                t_focb != t_locf 
+                and v_locf is not null 
+                and v_focb is not null
             then 
-                x1 + (
-                    (x2 - x1)
+                v_locf + (
+                    (v_focb - v_locf)
                     *
-                    (t - min(t) over imputed) /
-                    (t2 - (min(t) over imputed))
+                    (t - t_locf) /
+                    (t_focb - t_locf)
                 )
-            else coalesce(x1,x2)
+            else coalesce(v_locf, v_focb)
         end as v_final
     from tbl0
-    window imputed as (partition by a,b,x1)
     order by a,b,t
 )
 select * from tbl1 order by a,b,t;
