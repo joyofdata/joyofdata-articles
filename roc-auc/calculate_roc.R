16da949b71b5dd4cdca473e62d0b876bd9ebea57
rockclimber112358 <rockclimber112358@gmail.com>
2016-04-25 05:52:03 +0200

    Flexible column names and default equal costs
    This change allows users to use different column names for the prediction and target column names, and it allows users to run the code without explicitly specifying tp/fp costs (assuming they're equal).

--- roc-auc/calculate_roc.R
+++ roc-auc/calculate_roc.R
@@ -1,15 +1,29 @@
-calculate_roc <- function(df, cost_of_fp, cost_of_fn, n=100) {
+#' Calculate ROC
+#' 
+#' @param df The data.frame containing the predictions and true values.
+#' @param pred_column The column name of df containing the predictions.
+#' @param target_column The column name of df containing the target.
+#' @param cost_of_fp The cost of false positives.
+#' @param cost_of_fn The cost of false negatives.
+#' @param n The number of points to compute on the ROC curve.
+#' 
+#' @return A data.frame containing the ROC curve at n points.
+#' 
+
+calculate_roc <- function(df, pred_column="pred", target_column="survived",
+                          cost_of_fp=1, cost_of_fn=1, n=100) {
   tpr <- function(df, threshold) {
-    sum(df$pred >= threshold & df$survived == 1) / sum(df$survived == 1)
+    sum(df[[pred_column]] >= threshold & df[[target_column]] == 1) /
+          sum(df[[target_column]] == 1)
   }
   
   fpr <- function(df, threshold) {
-    sum(df$pred >= threshold & df$survived == 0) / sum(df$survived == 0)
+    sum(df[[pred_column]] >= threshold & df[[target_column]] == 0) / sum(df[[target_column]] == 0)
   }
   
   cost <- function(df, threshold, cost_of_fp, cost_of_fn) {
-    sum(df$pred >= threshold & df$survived == 0) * cost_of_fp + 
-      sum(df$pred < threshold & df$survived == 1) * cost_of_fn
+    sum(df[[pred_column]] >= threshold & df[[target_column]] == 0) * cost_of_fp + 
+      sum(df[[pred_column]] < threshold & df[[target_column]] == 1) * cost_of_fn
   }
   
   roc <- data.frame(threshold = seq(0,1,length.out=n), tpr=NA, fpr=NA)
